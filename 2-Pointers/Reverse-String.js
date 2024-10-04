b7fd2173ceeb12773afdf5043ed4ff316a5936f5
Asadbek <asadbekjon8899@gmail.com>
2024-10-04 22:19:20 +0500

    Reverse Strind problem solved

---
+++ 2-Pointers/Reverse-String.js
@@ -0,0 +1,7 @@
+const reverseString = function (s) {
+  const reversedS = s.reverse();
+  console.log(reversedS);
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
