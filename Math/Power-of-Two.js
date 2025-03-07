f5f39b38e88da06be3bf2285b852d9aa92040c4c
Asadbek <ibodullaev.asadbek@mail.ru>
2025-03-07 23:04:19 +0500

    solve

---
+++ Math/Power-of-Two.js
@@ -0,0 +1,11 @@
+var isPowerOfTwo = function (n) {
+  if (n % 2 === 0) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(4));
+console.log(isPowerOfTwo(3));
