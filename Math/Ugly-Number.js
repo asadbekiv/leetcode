326e3cdc8fb62274364598d9560babee41cf1ce9
Asadbek <ibodullaev.asadbek@mail.ru>
2025-03-12 23:45:25 +0500

    solve

---
+++ Math/Ugly-Number.js
@@ -0,0 +1,18 @@
+const isUgly = function (n) {
+  if (n < 1) return false;
+
+  while (n % 2 === 0) {
+    n /= 2;
+  }
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  while (n % 5 === 0) {
+    n /= 5;
+  }
+
+  return n === 1;
+};
+
+console.log(isUgly(6));
+console.log(isUgly(14));
