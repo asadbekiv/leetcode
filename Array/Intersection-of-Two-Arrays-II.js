e0622fad7f3f0b8abde0f3518bf78fdd221efd5d
Asadbek <ibodullaev.asadbek@mail.ru>
2025-03-15 14:20:58 +0500

    solve

--- Array/Intersection-of-Two-Arrays-II.js
+++ Array/Intersection-of-Two-Arrays-II.js
@@ -1,15 +1,15 @@
 var intersect = function (nums1, nums2) {
-  let allNumber = new Array();
-  let intersectNumbers = new Array();
-  if (nums1.length < nums2.length) {
-    [nums1, nums2] = [nums2, nums1];
+  let allNumber = [];
+
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums1.includes(nums2[i])) {
+      allNumber.push(nums2[i]);
+      nums1.splice(nums1.indexOf(nums2[i]), 1);
+    }
   }
 
-  for (let numbers1 of nums1) {
-    allNumber.push(numbers1S);
-  }
-  console.log(allNumber);
+  return allNumber;
 };
 
-console.log(intersection([1, 2, 2, 1], [2, 2]));
-console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
+console.log(intersect([1, 2, 2, 1], [2, 2]));
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
